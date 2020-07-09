eef873853a51ac40ba82338c34f1d07d65ecc359
Krzysztof <krzysztofs012@outlook.com>
2020-07-09 12:43:16 +0200

    React, redux, firebase connected. SCSS modules and basic tools (pretier, eslint) configuration completed.

---
+++ src/config/firebase-config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCNQkGxZOAxyv0blEI4_nDXAwDTCo610_0',
+  authDomain: 'csale-3d097.firebaseapp.com',
+  databaseURL: 'https://csale-3d097.firebaseio.com',
+  projectId: 'csale-3d097',
+  storageBucket: 'csale-3d097.appspot.com',
+  messagingSenderId: '1068574934795',
+  appId: '1:1068574934795:web:caf0959b44deec117ac40e',
+  measurementId: 'G-9HMSC78T61',
+};
+
+export default firebaseConfig;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
+/* eslint-disable no-undef */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+import firebase from 'firebase/app';
+import 'firebase/database';
+import 'firebase/firestore';
+
+import firebaseConfig from './config/firebase-config';
+
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, compose } from 'redux';
+import { firebaseReducer, ReactReduxFirebaseProvider } from 'react-redux-firebase';
+import { reduxFirestore, createFirestoreInstance, firestoreReducer } from 'redux-firestore';
+
 import App from './App';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
 
+firebase.initializeApp(firebaseConfig);
+
+const rootReducer = combineReducers({
+  firebase: firebaseReducer,
+  firestore: firestoreReducer,
+});
+
+const createStoreWithFirebase = compose(reduxFirestore(firebase))(createStore);
+
+const store = createStoreWithFirebase(rootReducer, {});
+
+const rrfConfig = {
+  userProfile: 'users',
+};
+
+const rrfProps = {
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
+};
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <App />
+    </ReactReduxFirebaseProvider>
+  </Provider>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
