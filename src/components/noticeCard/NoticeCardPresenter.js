c3314786384e18855bf1b647e77dd21cfb6eac02
Krzysztof <krzysztofs012@outlook.com>
2020-09-05 23:23:11 +0200

    Removed react fragment wrapper

--- src/components/noticeCard/NoticeCardPresenter.js
+++ src/components/noticeCard/NoticeCardPresenter.js
@@ -1,28 +1,18 @@
 import React, { Suspense } from 'react';
-import './NoticeCard.scss';
 import AppLoader from '../loader/Loader';
 import AuthorInfo from './AuthorInfo/AuthorInfo';
 import NoticeCardInfo from './NoticeCardInfo/NoticeCardInfo';
-import { connect } from 'react-redux';
 
 const NoticeCardPresenter = ({ notice, productCondition, userInfo }) => {
   return (
-    <>
-      <section className="noticeCard__wrapper">
-        <Suspense fallback={<AppLoader />}>
-          <img alt="Notice" className="noticeCard__photo" src={notice.productPhoto} />
-        </Suspense>
-        <NoticeCardInfo notice={notice} noticeCondition={productCondition} />
-        <AuthorInfo userInfo={userInfo} />
-      </section>
-    </>
+    <section className="noticeCard__wrapper">
+      <Suspense fallback={<AppLoader />}>
+        <img alt="Notice" className="noticeCard__photo" src={notice.productPhoto} />
+      </Suspense>
+      <NoticeCardInfo notice={notice} noticeCondition={productCondition} />
+      <AuthorInfo userInfo={userInfo} />
+    </section>
   );
 };
 
-const mapStateToProps = state => {
-  return {
-    userInfo: state.firebase.profile,
-  };
-};
-
-export default connect(mapStateToProps)(NoticeCardPresenter);
+export default NoticeCardPresenter;
