46e860a626fa0eab99deb1d58f6051822f56303a
Krzysztof <krzysztofs012@outlook.com>
2020-09-05 23:32:09 +0200

    Made and styled a header on mobile devices

--- src/shared/header/Header.js
+++ src/shared/header/Header.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import './Header.scss';
 import LogoutButton from '../../components/buttons/LogoutButton/LogoutButton.js';
 import { connect } from 'react-redux';
+import HeaderPresenter from './HeaderPresenter';
+import AboutButton from '../../components/buttons/AboutButton';
 import AddNoticeButton from '../../components/buttons/AddNoticeButton';
 import UserProfileButton from '../../components/buttons/UserProfileButton';
-import HeaderPresenter from './HeaderPresenter';
+import HomeButton from '../../components/buttons/HomeButton';
+import OptionsIcon from '../../components/buttons/OptionsIcon/OptionsIcon';
 
 const Header = ({ isProfileLoaded, userNick, loggedUser }) => {
   const isLogoutButtonRendered =
     isProfileLoaded && userNick ? (
-      <div className="header__options-wrapper">
-        <UserProfileButton />
-        <AddNoticeButton />
+      <>
+        <div className="header__options-wrapper">
+          <AboutButton />
+          <UserProfileButton />
+          <AddNoticeButton />
+        </div>
+        <HomeButton />
+        <OptionsIcon />
         <LogoutButton />
-      </div>
+      </>
     ) : (
-      <div></div>
+      <div className="header-options-wrapper">
+        <AboutButton />
+      </div>
     );
 
   const headerLogoRedirectPathName = loggedUser?.uid ? '/dashboard' : '/';
