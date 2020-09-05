8417406cfabfcb1e7ed3a5d01c3618f5ead1efbd
Krzysztof <krzysztofs012@outlook.com>
2020-09-05 23:21:45 +0200

    Fixed a bug with infinity queries

--- src/components/noticeCard/NoticeCard.js
+++ src/components/noticeCard/NoticeCard.js
@@ -1,25 +1,37 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { connect } from 'react-redux';
 import NoticeCardPresenter from './NoticeCardPresenter';
 import { renderNoticeCondition } from '../../utils/utilsFunctions';
 import LackOfNoticesMessage from '../../shared/LackOfNoticesMessage';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const getUser = async userId => {
+  return await firebase
+    .firestore()
+    .doc(`users/${userId}`)
+    .get()
+    .then(data => data.data());
+};
 
 const NoticeCard = ({ products }) => {
-  useEffect(() => window.scrollTo(0, 0), []);
+  const [user, setUser] = useState({});
   const { id } = useParams();
+  useEffect(() => window.scrollTo(0, 0), []);
 
   if (products !== undefined) {
     const [product] = products.filter(prod => prod.id === id);
+    if (Object.keys(user).length === 0) {
+      getUser(product.createdBy).then(data => setUser(data));
+    }
     const productCondition = renderNoticeCondition(product.productCondition);
-    return <NoticeCardPresenter productCondition={productCondition} notice={product} />;
+    return <NoticeCardPresenter productCondition={productCondition} notice={product} userInfo={user} />;
   } else return <LackOfNoticesMessage text="There is no notices." />;
 };
 
-const mapStateToProps = state => {
-  return {
-    products: state.firestore.ordered.notices,
-  };
-};
+const mapStateToProps = state => ({
+  products: state.firestore.ordered.notices,
+});
 
 export default connect(mapStateToProps)(NoticeCard);
