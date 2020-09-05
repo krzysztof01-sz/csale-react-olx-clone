51561a2243910ee8b6daefa872744eae197449b1
Krzysztof <krzysztofs012@outlook.com>
2020-09-05 23:16:27 +0200

    Added new icons

--- src/components/icons/icons.js
+++ src/components/icons/icons.js
@@ -7,12 +7,16 @@ import {
   faTrashAlt,
   faUserCircle,
   faEdit,
+  faInfoCircle,
+  faHome,
 } from '@fortawesome/free-solid-svg-icons';
 
 // static icons
 export const BasketIcon = () => <FontAwesomeIcon icon={faShoppingBasket} color="#f1ad3e" />;
 export const UploadIcon = () => <FontAwesomeIcon icon={faUpload} />;
 export const TrashIcon = () => <FontAwesomeIcon icon={faTrashAlt} color="#cdcdcd" />;
-export const HeaderPlusIcon = () => <FontAwesomeIcon className="headerIcon" icon={faPlusCircle} color="#cdcdcd" />;
-export const HeaderUserIcon = () => <FontAwesomeIcon className="headerIcon" icon={faUserCircle} color="#cdcdcd" />;
 export const EditIcon = () => <FontAwesomeIcon icon={faEdit} color="#f1ad3e" />;
+export const HeaderPlusIcon = () => <FontAwesomeIcon icon={faPlusCircle} className="headerIcon" color="#cdcdcd" />;
+export const HeaderUserIcon = () => <FontAwesomeIcon icon={faUserCircle} className="headerIcon" color="#cdcdcd" />;
+export const AboutIcon = () => <FontAwesomeIcon icon={faInfoCircle} className="headerIcon" color="#cdcdcd" />;
+export const HomeIcon = () => <FontAwesomeIcon icon={faHome} className="headerIcon" color="#cdcdcd" />;
