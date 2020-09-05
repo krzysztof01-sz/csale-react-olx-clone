a88e3e2a33153eac3938594faf047b0b37e51e1b
Krzysztof <krzysztofs012@outlook.com>
2020-09-05 23:19:18 +0200

    Changed rendering method

--- src/components/noticeCard/AuthorInfo/AuthorInfo.js
+++ src/components/noticeCard/AuthorInfo/AuthorInfo.js
@@ -4,9 +4,11 @@ const AuthorInfo = ({ userInfo }) => {
   return (
     <aside className="authorInfo">
       <h2 className="authorInfo__header">Owner information</h2> <br />
-      <div className="authorInfo-item">Nick: {userInfo.nick}</div>
-      <div className="authorInfo-item">Account balance: {userInfo.money}$</div>
-      <div className="authorInfo-item">Email address: {userInfo.email}</div>
+      <div className="authorInfo-item">Nick: {userInfo?.nick === undefined ? 'Loading' : userInfo.nick}</div>
+      <div className="authorInfo-item">
+        Account balance: {userInfo?.money === undefined ? 'Loading' : `${userInfo.money}$`}
+      </div>
+      <div className="authorInfo-item">Email address: {userInfo?.email === undefined ? 'Loading' : userInfo.email}</div>
     </aside>
   );
 };
